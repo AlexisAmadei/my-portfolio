3d8c4d5245d0322eb10f0f072a86acc67b631f15
Alexis Amadei <alexisamadei@gmail.com>
2024-04-05 16:45:44 +0200

    [UPDATE] add "work" part
    also include small css fix

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,11 @@ import {
   IconWordpress,
   IconFlutter,
 } from './assets/Icons';
+import { GitHub } from '@mui/icons-material'
+
+import previewPIM from './assets/preview/previewPIM.png'
+import previewFT2 from './assets/preview/previewFT2.png'
+import previewTinapp from './assets/preview/previewTinapp.png'
 
 export default function App() {
   const skills = [
@@ -33,6 +38,11 @@ export default function App() {
     { name: 'Wordpress', icon: IconWordpress },
     { name: 'Flutter', icon: IconFlutter },
   ]
+  const projects = [
+    { name: 'PIM', description: 'Projet Intensif Mobile', tech: ["React", "JS", "Firebase", "Git"], link:"https://github.com/AlexisAmadei/PIMobile", preview: previewPIM},
+    { name: 'FT2', description: 'Projet WebSchoolFactory dataviz', tech: ["React", "JS", "Git"], link:"https://github.com/AlexisAmadei/KEAPartners_reactApp", preview: previewFT2},
+    { name: 'Appli personnelle', description: 'Playground perso', tech: ["React", "firebase", "Postman", "Git"], link:"https://github.com/AlexisAmadei/tinapp", preview: previewTinapp},
+  ]
   return (
     <div className='app-wrapper'>
       <HomeHeader />
@@ -47,21 +57,38 @@ export default function App() {
       }} />
       <div className='home-content' id='aboutMe'>
         <div className='home-content-skill' id='skills'>
-          <h1 style={{ margin:0}}>Skills</h1>
-          <p style={{ margin:'1rem',width:'fit-content',color:'#0bbf6b',fontSize:'30px'}}>The skills, tools and technologies I know:</p>
+          <h1 style={{ margin: 0 }}>Skills</h1>
+          <p style={{ margin: '1rem', width: 'fit-content', color: '#0bbf6b', fontSize: '30px' }}>The skills, tools and technologies I know:</p>
           <div className='home-content-skill-icons'>
             {skills.map((skill, index) => (
               <div key={index} className='skill-item'>
-                  <skill.icon />
-                  <p>{skill.name}</p>
+                <skill.icon />
+                <p>{skill.name}</p>
               </div>
             ))}
           </div>
         </div>
+        <div className="separator" style={{
+          width: '100%',
+          height: '100px',
+        }} />
         <div className='home-content-project'>
-          <div className='home-content-project-1'></div>
-          <div className='home-content-project-2'></div>
-          <div className='home-content-project-3'></div>
+          <h1>Work</h1>
+          <div className='project-container'>
+            {projects.map((projet, index) => (
+              <div key={index} className='project-item'>
+                <img style={{borderRadius:'0.6rem'}} className='project-preview' src={projet.preview} width={"100%"} alt='project preview' />
+                <h2>{projet.name}</h2>
+                <p>{projet.description}</p>
+                <div className='project-tech-container'>
+                <a href={projet.link} target='_blank' rel='noreferrer'><p><GitHub style={{ color:"white" }} /></p></a>
+                  {projet.tech.map((tech, index) => (
+                    <span key={index} className='project-tech'>{tech}</span>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
         <div className='home-content-experience'>
           <div className='home-content-experience-1'></div>
