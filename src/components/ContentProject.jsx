867da84ff20ff49835bfc5bb974f134837f12fed
Alexis Amadei <alexisamadei@gmail.com>
2024-04-09 20:31:59 +0200

    [EDIT] edit details on Playground

--- src/components/ContentProject.jsx
+++ src/components/ContentProject.jsx
@@ -9,7 +9,7 @@ export default function ContentProject() {
   const projects = [
     { name: 'PIM', description: 'Projet Intensif Mobile', tech: ["React", "JS", "Firebase", "Git"], link: "https://github.com/AlexisAmadei/PIMobile", preview: previewPIM },
     { name: 'FT2', description: 'Projet WebSchoolFactory dataviz', tech: ["React", "JS", "Git"], link: "https://github.com/AlexisAmadei/KEAPartners_reactApp", preview: previewFT2 },
-    { name: 'Appli personnelle', description: 'Playground perso', tech: ["React", "firebase", "Postman", "Git"], link: "https://github.com/AlexisAmadei/tinapp", preview: previewTinapp },
+    { name: 'Tinapp', description: 'Playground', tech: ["React", "JS", "Firebase", "Postman", "Git"], link: "https://github.com/AlexisAmadei/tinapp", preview: previewTinapp },
   ]
   return (
     <div className='home-content-project'>
