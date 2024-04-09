c89b8e27d9d519972ee680eb42351f337cd4b3ee
Alexis Amadei <alexisamadei@gmail.com>
2024-04-09 20:03:42 +0200

    [EDI] change design of experience title

--- src/components/ContentExp.jsx
+++ src/components/ContentExp.jsx
@@ -6,14 +6,15 @@ import { LocationOn } from '@mui/icons-material';
 export default function ContentExp() {
   const experiences = [
     {
-      name: 'Frontend developer @ Accor',
+      name: 'Frontend developer',
+      company:'Accor',
       date: '2023 (2 months)',
       description: 'Lorem ipsum dolor sit amet',
       location: 'Issy-les-Moulineaux, France',
       skills: ['TypeScript', 'Python', 'GitLab', 'Splunk', 'Leaflet'],
     },
     {
-      name: 'This portfolio ',
+      name: 'This Portfolio',
       date: '∞',
       description: `The website your eyes are currently on`,
       location: 'Paris, France',
@@ -25,7 +26,6 @@ export default function ContentExp() {
     const updatedShowDetails = [...showDetails];
     updatedShowDetails[index] = !showDetails[index];
     setShowDetails(updatedShowDetails);
-    console.log('details => ', showDetails);
   };
 
   return (
@@ -36,7 +36,11 @@ export default function ContentExp() {
       {experiences.map((exp, index) => (
         <div key={index} className='experience-item'>
           <div className='experience-item-preview'>
-            <h2>{exp.name}</h2>
+            <h2>
+              {exp.name}
+              {exp?.company !== undefined && exp?.company !== '' ? ' at ' : exp?.company}
+              <span style={{ color: 'var(--color-accent)' }}>{exp?.company}</span>
+            </h2>
             <div className='experience-item-preview-rightSide'>
               <p style={{ fontSize: 'larger' }}>{exp.date}</p>
               <span style={{ cursor:'pointer' }} onClick={() => handleToggleDetails(index)}>
@@ -50,10 +54,10 @@ export default function ContentExp() {
           </div>
           {showDetails[index] ? (
             <div className='experience-item-details'>
-              <p id='location'><LocationOn />{exp?.location}</p>
-              <p id='description'>{exp?.description}</p>
+              <p id='location'><LocationOn />{exp.location}</p>
+              <p id='description'>{exp.description}</p>
               <div className='experience-item-skills'>
-                {exp?.skills.map((skill, index) => (
+                {exp.skills.map((skill, index) => (
                   <p key={index}>{skill}</p>
                 ))}
               </div>
