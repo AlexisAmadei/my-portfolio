7938dbf3636e57212db8be44bc7e7a4d88d019d1
Alexis Amadei <alexisamadei@gmail.com>
2024-04-28 20:23:19 +0200

    [UPDATE] Ajout de l'importation de la fonction getFirestore dans firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -1,10 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getPerformance } from "firebase/performance";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDtWbNE1kE0_7u3SEhUliVCoztaiY_p4Dw",
   authDomain: "alexisa-portfolio.firebaseapp.com",
@@ -14,8 +13,8 @@ const firebaseConfig = {
   appId: "1:174339247120:web:3f406616896a5f9d164e95"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const perf = getPerformance(app);
 
 export { app, perf };
